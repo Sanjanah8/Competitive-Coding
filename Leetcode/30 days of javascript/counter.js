03e7a8a8147b5033fa3735c21de6e7920f260171
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-07 21:24:55 +0530

    Create counter.js

---
+++ Leetcode/30 days of javascript/counter.js
@@ -0,0 +1,13 @@
+var createCounter = function(n) {
+    
+    return function() {
+        
+    };
+};
+
+ var createCounter = function(n){
+    return function() {
+        return n++;
+    }
+
+ };
