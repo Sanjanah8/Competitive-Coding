1f59fbc38de701cd0034b0367a012e9c32b8cbfa
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-07 21:28:28 +0530

    Create binary search.java

---
+++ Leetcode/Binary search/binary search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] n, int target) {
+        int m = -1;
+
+        for(int i=0; i<n.length; i++){
+            if(n[i]==target){
+                m=i;
+            }
+        }
+        if(m==-1){
+            return m;
+        }else{
+            return m;
+        }   
+    }
+}
