bdfe49351a2d2b29e0ba6e520b14a29b00418c99
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-14 10:13:13 +0530

    Update convert string to array.java

--- Skillrack/java/starter/convert string to array.java
+++ Skillrack/java/starter/convert string to array.java
@@ -1,11 +1,10 @@
+1)
+    
 import java.util.Scanner;
-
 public class StringToCharArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String str = sc.nextLine(); // Read input string
-
         char[] charArr = str.toCharArray(); 
 
         for (char ch : charArr) { 
@@ -13,5 +12,25 @@ public class StringToCharArray {
         }
 
         sc.close(); // Close scanner
+    }
+}
+
+2)
+    
+import java.util.Scanner;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        char[] charArray = str.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            System.out.println(charArray[i]);
+        }
+
+        sc.close();
     }
 }
