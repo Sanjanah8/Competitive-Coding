17b905abcdb94bf535a6de1d1c3780af1c7ccb31
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-14 09:45:56 +0530

    Create convert character array to string.java

---
+++ Skillrack/java/starter/convert character array to string.java
@@ -0,0 +1,7 @@
+public class ConvertCharArray {
+    public static void main(String[] args) {
+        char[] ARR = {'s','k', 'i', 'l', 'l', 'r','a','c','k'};
+        String STR = new String(ARR);
+        System.out.println(STR);
+    }
+}
