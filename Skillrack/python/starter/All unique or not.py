98a2b28c0426053f76829e58fa7d19ce2e47205c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-16 13:00:30 +0530

    Create All unique or not.py

---
+++ Skillrack/python/starter/All unique or not.py
@@ -0,0 +1,8 @@
+1) numList = [int(val) for val in input().split()]
+print("YES" if len(numList) == len(set(numList)) else "NO")
+
+2)numList = list(map(int, input().split()))
+if len(numList) == len(set(numList)):
+    print("YES")
+else:
+    print("NO")
