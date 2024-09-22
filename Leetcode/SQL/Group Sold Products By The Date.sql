b56cd02f0fbdfc6351fc99706ee8ceef22644ad7
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-22 09:45:13 +0530

    Update Group Sold Products By The Date.sql

--- Leetcode/SQL/Group Sold Products By The Date.sql
+++ Leetcode/SQL/Group Sold Products By The Date.sql
@@ -1,5 +1,10 @@
-select sell_date, count(distinct product) num_sold
-    , string_agg(product, ',') within group( order by product) products
+SELECT  sell_date, count( distinct product) num_sold, 
+        GROUP_CONCAT(distinct product ORDER BY product SEPARATOR ',') products
+FROM activities
+GROUP BY 1
+ORDER BY 1;
+
+select sell_date, count(distinct product) num_sold,string_agg(product, ',') within group( order by product) products
 from (select distinct * from Activities) t
 group by sell_date
 order by sell_date
