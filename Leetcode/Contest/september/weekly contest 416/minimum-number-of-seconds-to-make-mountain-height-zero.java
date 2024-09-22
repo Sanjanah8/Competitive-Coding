48875356076e91459614b903a0076419aba08a5f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-22 08:58:37 +0530

    Update minimum-number-of-seconds-to-make-mountain-height-zero.java

--- Leetcode/Contest/september/weekly contest 416/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ Leetcode/Contest/september/weekly contest 416/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,1 +1,36 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        long totalSeconds = 0;
+        int n = workerTimes.length;
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+        
+        for (int i = 0; i < n; i++) {
+            pq.offer(new long[]{workerTimes[i], 1, i}); 
+        }
+
+        while (mountainHeight > 0) {
+            long[] current = pq.poll();
+            long currentTime = current[0];
+            int currentHeight = (int) current[1];
+            int workerIndex = (int) current[2];
+
+            totalSeconds = Math.max(totalSeconds, currentTime);
+            mountainHeight--;
+
+            pq.offer(new long[]{currentTime + workerTimes[workerIndex] * (currentHeight + 1), currentHeight + 1, workerIndex});
+        }
+
+        return totalSeconds;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minNumberOfSeconds(4, new int[]{2, 1, 1})); 
+        System.out.println(solution.minNumberOfSeconds(10, new int[]{3, 2, 2, 4})); 
+        System.out.println(solution.minNumberOfSeconds(5, new int[]{1}));
+    }
+}
 
