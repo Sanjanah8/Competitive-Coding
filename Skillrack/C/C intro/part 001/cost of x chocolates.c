f362b834d5497a32ce9db2287c3499bc5f065abd
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-24 11:42:04 +0530

    Update cost of x chocolates.c

--- Skillrack/C/C intro/part 001/cost of x chocolates.c
+++ Skillrack/C/C intro/part 001/cost of x chocolates.c
@@ -1,4 +1,5 @@
-//There are X chocolates to be bought for a birthday, where each chocolate costs Y rupees. The program must print the total amount required to buy X chocolates as the output
+//There are X chocolates to be bought for a birthday, where each chocolate costs Y rupees. 
+//The program must print the total amount required to buy X chocolates as the output
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -6,7 +7,7 @@
 int main()
 {
 int x,y;
-scanf("%d %d",*x,&y);
+scanf("%d %d",&x,&y);
 printf("%d",x*y);
 return 0;
 }
