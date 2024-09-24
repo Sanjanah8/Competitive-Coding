a177a7a31cfb616f2e932493228df95bcefc55ab
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-24 11:47:52 +0530

    Create count of legs.c

---
+++ Skillrack/C/C intro/part 001/count of legs.c
@@ -0,0 +1,12 @@
+//total number of legs of birds and animals
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int birds,animals;
+scanf("%d %d",&birds,&animals);
+printf("%d",(birds*2)+(animals*4));
+return 0;
+}
