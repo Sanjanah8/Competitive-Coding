952011fe58c643bdc574c14cdc9f327c3665cfa6
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-24 11:37:14 +0530

    Create solve equation with five integers.c

---
+++ Skillrack/C/C intro/part 001/solve equation with five integers.c
@@ -0,0 +1,12 @@
+//(4 * 5) + (20 * 2) + 10 = 70
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int a,b,c,d,e;
+scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+printf("%d",(a*b)+(d*e)+c);
+return 0;
+}
