0fab645dacf1e3aa99b7df1e83f08f56a1141659
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-24 11:53:22 +0530

    Create count of distributed icecreams.c

---
+++ Skillrack/C/C intro/part 001/count of distributed icecreams.c
@@ -0,0 +1,11 @@
+/*Fareen purchased X icecream(s) for a group of Y person(s). The program must calculate the number of icecream(s) given to each person in the group.*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int x,y;
+scanf("%d %d",&x,&y);
+printf("%d",x/y);
+return 0;
+}
