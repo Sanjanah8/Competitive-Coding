fff259e9d118a610ed2ef617c50182955de60684
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-24 11:41:25 +0530

    Create cost of x chocolates.c

---
+++ Skillrack/C/C intro/part 001/cost of x chocolates.c
@@ -0,0 +1,12 @@
+//There are X chocolates to be bought for a birthday, where each chocolate costs Y rupees. The program must print the total amount required to buy X chocolates as the output
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int x,y;
+scanf("%d %d",*x,&y);
+printf("%d",x*y);
+return 0;
+}
