007e2db719a56757a960184d3787d80069076822
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-27 12:12:44 +0530

    Create check if direction is n,e,w,s.c

---
+++ Skillrack/C/C intro/part 002/check if direction is n,e,w,s.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char ch;
+scanf("%c",ch);
+if(ch=='N'){
+    printf("North");
+}
+else if(ch=='E'){
+    printf("East");}
+else if(ch=='W'){
+    printf("West");
+}
+else if(ch=='S')
+{
+    printf("South");
+}
+else{
+    printf("Invalid");
+}
+}return 0;
+}
