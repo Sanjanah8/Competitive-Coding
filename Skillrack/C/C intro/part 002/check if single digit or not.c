4f53dad35de89500a6beac81b4fad36ee338660b
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-27 11:42:03 +0530

    Create check if single digit or not.c

---
+++ Skillrack/C/C intro/part 002/check if single digit or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n;
+scanf("%d",&n);
+switch(n){
+    case 0...9:
+    printf("yes");
+    break;
+    default:
+    printf("no");
+}
+return 0;
+}
