21f518256bff1aef9340723345a97f91ba497d32
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-28 08:31:30 +0530

    Create 2.c

---
+++ Skillrack/C/C intro/part 003/smallest even digit/single number/2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    
+    n > 2 ? printf("Smallest even number is 2\n") : printf("Smallest even number is 2\n");
+    
+    return 0;
+}
