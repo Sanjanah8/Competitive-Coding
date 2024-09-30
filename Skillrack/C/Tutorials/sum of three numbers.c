ccf8f48ebb4c64a9e8f94d2a97aba2f63d7924d4
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-30 12:44:55 +0530

    Create sum of three numbers.c

---
+++ Skillrack/C/Tutorials/sum of three numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int first,second,third;
+ scanf("%d",&first);
+ scanf("%d",&second);
+ scanf("%d",&third);
+ printf("%d",first+second+third);
+    return 0;
+}
