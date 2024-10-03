41c6b1dca7770df7dc6b3c7cdb1b55c89bab26d2
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-10-03 12:16:29 +0530

    Create interface odd or even a to b.java

---
+++ Skillrack/java/very easy/interface odd or even a to b.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int odd,even;
+if(b%2==0){
+    even=b;
+}else{ even=b-1;}
+
+	}
+}
