a126043af11e7e2ab4d5b28df08e8a4517b6569e
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-10-07 10:27:25 +0530

    Create equal sum.c

---
+++ Skillrack/C/if else/equal sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n1,n2,n3,n4;
+scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
+if(n1+n2==n3+n4)
+{printf("Yes");
+}
+else{
+    printf("No");
+}
+return 0;
+}
