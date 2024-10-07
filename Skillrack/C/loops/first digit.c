9e61581579d8bcb33d7f921b1aeb98f8dba86be0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-10-07 10:22:30 +0530

    Create first digit.c

---
+++ Skillrack/C/loops/first digit.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n,flag=-;
+scanf("%d", &n);
+if(n<0){
+    n=n*-1;
+    flag=1;
+}
+while(n>10){
+    n=n/10;
+}
+if(flag==0){
+    printf("%d",n);
+}
+else{
+    printf("-%d",n);
+}
+return 0;
+}
