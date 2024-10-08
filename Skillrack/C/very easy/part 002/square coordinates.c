45ba17563db95ca240cf02ae8efa6c35e0d44814
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-10-08 12:21:17 +0530

    Create square coordinates.c

---
+++ Skillrack/C/very easy/part 002/square coordinates.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int bottomleftx,bottomlefty,length;
+scanf("%d%d",&bottomleftx,&bottomlefty,&length);
+int toprightx=bottomleftx+length;
+int toprighty=bottomleftx=length;
+printf("%d\n%d",toprightx,toprighty);
+}
