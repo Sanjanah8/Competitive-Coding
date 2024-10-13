95a84273d3ddae5b1209ec3c1886d73081d2c2e1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-10-13 12:02:23 +0530

    Create yes or no.c

---
+++ Skillrack/C/very easy/part 003/yes or no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char c;
+c=getchar();
+if(c=='y'+||c=='Y'){
+    printf("yes");
+}else{
+    if(c=='n'||c=='N'){
+        printf("no");
+    }else{
+        printf("invalid");
+    
+}
+}
