abba443506ae17621ebf4f01968876677f23df29
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-28 19:39:23 +0530

    Create transpose matrix.c

---
+++ Skillrack/C/C intro/part 004/transpose matrix.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int r,c,row,col;
+scanf("%d %d",&r,&c);
+int mat[row][col];
+for(row=0;row<r;row++){
+    for(col=0;col<c;col++)
+{
+    scanf("%d",&mat[row][col]);
+}}
+for(col=0;col<c;col++){
+    for(row=0;row<r;row++){
+        printf("%d ",mat[row][col]);
+}printf("\n");
+}
+}return 0;
+}
