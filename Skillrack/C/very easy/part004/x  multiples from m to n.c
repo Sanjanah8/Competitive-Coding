d990394cc32d89a3b0c085357a1b5d42866f5ef0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-28 21:03:53 +0530

    Create x  multiples from m to n.c

---
+++ Skillrack/C/very easy/part004/x  multiples from m to n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int m,n,x;
+scanf("%d%d%d",&m,&n,&x);
+int ctr=n;
+while(ctr>=n){
+    if(ctr%x==0){
+        printf("%d ",ctr);
+    }ctr==;
+}
+}
