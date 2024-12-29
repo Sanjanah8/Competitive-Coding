75127bb9b0961d7d23a2d8f44f0426135d6d5fe3
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-29 21:40:35 +0530

    Create divisibility of integers.c

---
+++ Skillrack/C/loops/divisibility of integers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n1,n2,n3;
+scanf("%d %d %d",&n1,&n2,&n3);
+if((n1*n2*n3)%(n1+n+n3)==0){
+    printf("Yes");
+}else{
+    printf("No");
+}
+return 0;
+}
