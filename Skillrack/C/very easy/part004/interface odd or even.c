6ed5aff0ad83ede5d270b7c32770ea334b150664
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 19:02:50 +0530

    Create interface odd or even.c

---
+++ Skillrack/C/very easy/part004/interface odd or even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int x,y,i,j;
+scanf("%d %d",&x,&y);
+for(i=x,y=y;i<=y;i++;j--){
+    if(i%2==1){
+        printf("%d ",i);
+    }if(j%2==0){
+        printf("%d ",j);
+}}
+}
