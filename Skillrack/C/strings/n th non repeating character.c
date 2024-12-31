a530342e28623da62e7ce190ee1710b898faade7
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 13:48:39 +0530

    Create n th non repeating character.c

---
+++ Skillrack/C/strings/n th non repeating character.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+char str[101];
+int len,count=0,n,charcount[128]={0};
+scanf("%s",str);
+len=strlen(str);
+scanf("\n%d",&n);
+for(int i=0;i<len;i++){
+    charcount[str[i]]++;
+}for(int i=0;i<len;i++){
+    if(charcount[str[i]]==1){
+        count++;
+        if(charcount==n){
+            printf("%c",str[i]);
+            return 0;
+        }
+    }
+}
+}
