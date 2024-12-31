b40684c33738b199c71905e0ef0d76ff01fbf19f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 19:12:35 +0530

    Create except multiples of n.c

---
+++ Skillrack/C/very easy/part004/except multiples of n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+int count=a;
+while(count<=b){
+    if(count%n!=0){
+        printf("%d ",count);
+    }count++;
+}
+}
