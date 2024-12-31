f45f9b3dd5ab590e538a31437a90a453a8e8b9b1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 19:04:51 +0530

    Update interface odd or even.c

--- Skillrack/C/very easy/part004/interface odd or even.c
+++ Skillrack/C/very easy/part004/interface odd or even.c
@@ -4,11 +4,14 @@
 int main()
 {
 int x,y,i,j;
-scanf("%d %d",&x,&y);
-for(i=x,y=y;i<=y;i++;j--){
+scanf("%d%d",&x,&y);
+for(i=x,j=y;i<=y;i++,j--)
+{
     if(i%2==1){
         printf("%d ",i);
     }if(j%2==0){
         printf("%d ",j);
-}}
+}
+    
+}
 }
