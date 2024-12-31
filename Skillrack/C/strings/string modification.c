17323ff39b2a1713bad003e356277463fe86ce20
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 14:01:45 +0530

    Update string modification.c

--- Skillrack/C/strings/string modification.c
+++ Skillrack/C/strings/string modification.c
@@ -8,7 +8,7 @@ scanf("%s",str);
 int i=0;
 while(str[i]!='\0'){
     if(str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u'){
-        printf("%.c",str[i]);
+        printf(".%c",str[i]);
     }i++;
 }return 0;
 }
