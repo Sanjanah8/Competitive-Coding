a9a02b9f8fe14ad68a05acf90345292780faa10c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-12-31 13:51:47 +0530

    Update n th non repeating character.c

--- Skillrack/C/strings/n th non repeating character.c
+++ Skillrack/C/strings/n th non repeating character.c
@@ -4,17 +4,18 @@
 int main()
 {
 char str[101];
-int len,count=0,n,charcount[128]={0};
+int len,count=0,n,charcount[128]={0};;
 scanf("%s",str);
 len=strlen(str);
 scanf("\n%d",&n);
 for(int i=0;i<len;i++){
     charcount[str[i]]++;
-}for(int i=0;i<len;i++){
+}
+for(int i=0;i<len;i++){
     if(charcount[str[i]]==1){
         count++;
-        if(charcount==n){
-            printf("%c",str[i]);
+        if(count==n){
+            printf("%c", str[i]);
             return 0;
         }
     }
