de3bfa5e3aaa974137fd5b5f809ab3a699a51dd0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-01 13:44:02 +0530

    Update prime.py

--- Skillrack/python/easy/prime.py
+++ Skillrack/python/easy/prime.py
@@ -3,7 +3,7 @@ def prime(n):
         return False
     for i in range(2,int(n**0.5)+1):
         if n5i==0:
-        return False
+           return False
     return True
 n=int(input())
 if prime(n):
