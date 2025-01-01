1c8fd88600d4a725a767b7187f46a0dda760a342
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-01 16:45:02 +0530

    Update leap year.py

--- Skillrack/python/easy/leap year.py
+++ Skillrack/python/easy/leap year.py
@@ -1,7 +1,9 @@
 y=int(input())
-if(y%400==0):
+if y%400==0:
     print("yes")
-elif(y%100==0):
+elif y%100==0:
     print("no")
-elif(y%4==0):
+elif y%4==0:
     print("yes")
+else:
+    print("no")
