6b356a3f934126350cf2bc9ef232055a7b203e00
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-01 13:18:33 +0530

    Update count of full scale.c

--- Skillrack/C/loops/count of full scale.c
+++ Skillrack/C/loops/count of full scale.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-int f,h;
+int f=0,h=0;
 char ch;
 do{
     scanf("%c",&ch);
@@ -12,8 +12,11 @@ do{
     }else if(ch=='S'){
         h++;
     }
-}while(ch!='\r'&&ch!='\n');{
+}
+while(ch!='\r'&&ch!='\n');
     printf("%d",f+h/2);
     return 0;
 }
-}
+/* The program must accept a sequence of alphabets S and T as the input where T represents full scale and S represents the half scale of a measuring unit. 
+The two half scale units can be considered as 1 full scale unit. The program must print the count of the full scale units as the output.
+Example Input/Output 1: Input: STSTSTT Output: 5 Example Input/Output 2: Input: TSSSSSTTTTS Output: 8 */
