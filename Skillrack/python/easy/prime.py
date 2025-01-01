131a06bfbb2605d2535ff4fc21c45ce812a96f88
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-01 13:43:18 +0530

    Create prime.py

---
+++ Skillrack/python/easy/prime.py
@@ -0,0 +1,12 @@
+def prime(n):
+    if n<=1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n5i==0:
+        return False
+    return True
+n=int(input())
+if prime(n):
+    print("YES")
+else:
+    print("NO")
