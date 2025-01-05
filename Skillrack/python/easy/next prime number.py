fe6a03f51f459109461765414f5a88c6e12941d3
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-05 21:49:31 +0530

    Update next prime number.py

--- Skillrack/python/easy/next prime number.py
+++ Skillrack/python/easy/next prime number.py
@@ -1,10 +1,10 @@
 def is_prime(n):
-    if num<2:
+    if n<2:
         return False
-    for i in range(2,int(num**0.5)+1):
-        if num%i==0:
+    for i in range(2, int(n**0.5) +1):
+        if n%i==0:
             return False
-        return True
+    return True
 n=int(input())
 n+=1
 while not is_prime(n):
