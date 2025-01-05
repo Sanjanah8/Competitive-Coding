1839351f8a5f2f580fb8424e39b29f16ed4c6532
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-05 20:43:46 +0530

    Update palindrome missing alphabet.py

--- Skillrack/python/easy/palindrome missing alphabet.py
+++ Skillrack/python/easy/palindrome missing alphabet.py
@@ -1,5 +1,12 @@
 s=input()
-for i in range(len(s)//2):
-    if s[i]!=s[-(i+1)]:
-        print(s[i])
-        break
+l=list(s)
+j=len(l)-1
+for i in range(len(l)):
+    if l[i]!=l[j]:
+        if l[i]!=l[j-1] and i!=j-1:
+            print(l[j])
+            break
+        else:
+            print(l[i])
+            break
+    j-=1
