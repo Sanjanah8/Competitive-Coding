40183264a8fd873341ecd4d0a9cc1cf05b34a99f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-11 14:56:53 +0530

    Create sum of array elements.c

---
+++ Skillrack/C/arrays/sum of array elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n;
+scanf("%d",&n);
+int arr[n];
+for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+for(int i=0;i<n/2;i++){
+    printf("%d ",arr[i]+arr[n-i-1]);
+}
+if(n%2!=0){
+    printf("%d ",arr[n/2]);
+}
+}
