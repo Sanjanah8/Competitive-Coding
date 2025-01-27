7efa763531830083c1a4e883c3a0a07378b34725
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-27 12:04:27 +0530

    Create calendar month.py

---
+++ Skillrack/python/easy/calendar month.py
@@ -0,0 +1,4 @@
+date=input().strip()
+month=int(date.split('-')[1])
+months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+print(months[month-1])
