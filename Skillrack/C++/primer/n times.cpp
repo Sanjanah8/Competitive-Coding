d135fa94df1de153ab7efd23e5c55499a870fad2
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-03-02 12:37:35 +0530

    Create n times.cpp

---
+++ Skillrack/C++/primer/n times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    int counter = 1;
+    while(counter <= N)
+    {
+        cout << "SkillRack" << endl;
+        counter++;
+    }
+    return 0;
+}
