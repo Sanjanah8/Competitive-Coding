ebc8edbf08fe777572fdf3bc3f8accb14805c34d
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-03-05 09:46:35 +0530

    Create maximum.cpp

---
+++ Skillrack/C++/primer/maximum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    if (num1 >= num2 && num1 >= num3) {
+        cout << num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        cout << num2;
+    } else {
+        cout << num3;
+    }
+    return 0;
+}
