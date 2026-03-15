ce1c169013e912656a2b8d0dc7e036384ebe2463
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-03-15 20:49:34 +0900

    Create longest valid parantheses.py

---
+++ Skillrack/Daily challenge or test/daily test/longest valid parantheses.py
@@ -0,0 +1,13 @@
+s=input().strip()
+stack=[-1]
+max_len=0
+for i in range(len(s)):
+    if s[i]=='(':
+        stack.append(i)
+    else:
+        stack.pop()
+        if len(stack)==0:
+            stack.append(i)
+        else:
+            max_len=max(max_len,i-stack[-1])
+print(max_len)
