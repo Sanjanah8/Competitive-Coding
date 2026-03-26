390317f0b0cad07877ad64269b0eaf7130168417
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-03-26 10:19:12 +0530

    Create x to y.cpp

---
+++ Skillrack/C++/primer/x to y.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+
+    // Fill in the blank below:
+    for (int counter = X; counter <= Y; counter++)
+    {
+        cout << counter << " ";
+    }
+    
+    return 0;
+}
