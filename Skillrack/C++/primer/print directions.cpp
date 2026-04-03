0a12576a1984ece42b9a859fc4216853e2e3a98c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-04 02:41:46 +0900

    Create print directions.cpp

---
+++ Skillrack/C++/primer/print directions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char dir[] = {'N', 'E', 'W', 'S'}; // First Blank
+
+    for (char d : dir)
+    {
+        cout << d << " "; // Second Blank
+    }
+    
+    return 0;
+}
