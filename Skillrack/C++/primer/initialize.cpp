ee120284d94240d0dedca9a1a7bd4a056bf95e06
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-04 02:43:55 +0900

    Create initialize.cpp

---
+++ Skillrack/C++/primer/initialize.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Blank 1: 6, Blank 2: 20,
+    int arr[6] = { 20, 45 }; 
+
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}
