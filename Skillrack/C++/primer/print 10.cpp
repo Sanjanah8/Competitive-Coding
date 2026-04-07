52f6eaff07cff1236c020ab90f70a9c11b8934c1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-07 15:01:06 +0530

    Create print 10.cpp

---
+++ Skillrack/C++/primer/print 10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printTen()
+{ 
+    int val = 10; // This is the line for the blank
+    cout << val << " ";
+    val++;
+}
+
+int main()
+{ 
+    printTen(); 
+    printTen(); 
+    printTen();
+    return 0;
+}
