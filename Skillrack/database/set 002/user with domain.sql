7441e75871639e6fab7a15619110e7c8e7ecd70f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-08 23:17:28 +0900

    Update user with domain.sql

--- Skillrack/database/set 002/user with domain.sql
+++ Skillrack/database/set 002/user with domain.sql
@@ -1,11 +1,17 @@
+SET BUILTIN_ALIAS_OVERRIDE=TRUE;
+
 SELECT 
     u.id, 
     u.name, 
     d.id, 
     d.name
 FROM 
-    user u
-JOIN 
+    "user" u
+LEFT JOIN 
     domain d ON u.domainid = d.id
 ORDER BY 
     u.id DESC;
+
+
+ set non_keywords user;
+ select u.id,u.name,d.id,d.name from "USER" u left join domain d on u.domainid=d.id order by u.id desc;
