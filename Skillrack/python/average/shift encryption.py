0ec41f0e23a30eea4458095ce29de8dc0a04ed29
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-09 23:36:54 +0900

    Update shift encryption.py

--- Skillrack/python/average/shift encryption.py
+++ Skillrack/python/average/shift encryption.py
@@ -1,5 +1,5 @@
 m,x,y=input(),int(input()),int(input())
 for c in m:
  if c.islower():c=chr((ord(c)-97+x)%26+97)
- if c.isdigit():c=str((int(c)+y)%10)
+ elif c.isdigit():c=int(c)+y
  print(c,end="")
