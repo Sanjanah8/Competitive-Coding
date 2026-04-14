4048666c8bd54357702a6e2a99000f4ca03d6df1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-14 23:29:43 +0900

    Create k largest integers.py

---
+++ Skillrack/datastructures/heap/k largest integers.py
@@ -0,0 +1,12 @@
+import sys
+import heapq
+
+data = sys.stdin.read().split()
+if not data:
+    exit()
+
+n = int(data[0])
+arr = list(map(int, data[1:-1]))
+k = int(data[-1])
+
+print(*(heapq.nlargest(k, arr)))
