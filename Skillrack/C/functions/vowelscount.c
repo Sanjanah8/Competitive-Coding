832f027055cdbc61d06bf32a26b560259cd1774a
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-16 09:56:04 +0530

    Create vowelscount.c

---
+++ Skillrack/C/functions/vowelscount.c
@@ -0,0 +1,9 @@
+int getVowelsCount(char *str) {
+    int count = 0;
+    for (int i = 0; str[i]; i++) {
+        char c = tolower(str[i]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            count++;
+    }
+    return count;
+}
