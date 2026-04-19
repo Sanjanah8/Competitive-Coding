6bab7d00d4861bb6ed479dfa0ec0d7ca94a1578f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-04-19 18:52:05 +0900

    Create fibonacci memoization.cpp

---
+++ Leetcode/c++/fibonacci memoization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (dp[n] != -1) return dp[n];
+
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1, -1); 
+        return solve(n, dp);
+    }
+};
