3969a8b9390461287cffbf3dfa228e6949ca1d5e
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-06-16 15:38:41 +0530

    Update shuffle string.py

--- Skillrack/LACS/shuffle string.py
+++ Skillrack/LACS/shuffle string.py
@@ -1,21 +1,9 @@
-s1 = input().strip()
-s2 = input().strip()
+s = input().strip()
+indices = list(map(int, input().split()))
 
-if len(s1) != len(s2):
-    print("false")
-else:
-    diff = []
-    for i in range(len(s1)):
-        if s1[i] != s2[i]:
-            diff.append(i)
-            
-    if len(diff) == 0:
-        print("true")
-    elif len(diff) == 2:
-        i, j = diff[0], diff[1]
-        if s1[i] == s2[j] and s1[j] == s2[i]:
-            print("true")
-        else:
-            print("false")
-    else:
-        print("false")
+shuffled = [''] * len(s)
+
+for i in range(len(s)):
+    shuffled[indices[i]] = s[i]
+
+print("".join(shuffled))
