c761e3984aeaa8477dbafd93e237e8afb2d1a267
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-06-21 16:49:23 +0900

    Update count squares.cpp

--- Geeks For Geeks/basic/count squares.cpp
+++ Geeks For Geeks/basic/count squares.cpp
@@ -4,3 +4,14 @@ class Solution {
         return sqrt(n-1);
     }
 };
+// for understanding
+class Solution {
+  public:
+    int countSquares(int n) {
+        long long i = 1;
+        while (i * i < n) {
+            i++;
+        }
+        return i - 1; 
+    }
+};
