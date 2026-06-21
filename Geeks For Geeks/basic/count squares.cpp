1cde09e9afb8a3998a59eec9427592af94477f89
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-06-21 16:50:07 +0900

    Update count squares.cpp

--- Geeks For Geeks/basic/count squares.cpp
+++ Geeks For Geeks/basic/count squares.cpp
@@ -15,3 +15,25 @@ class Solution {
         return i - 1; 
     }
 };
+
+//manual
+
+class Solution {
+  public:
+    int countSquares(int n) {
+        long long target = n - 1;
+        long long low = 0, high = target;
+        long long ans = 0;
+        
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (mid * mid <= target) {
+                ans = mid;   
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return ans;
+    }
+};
