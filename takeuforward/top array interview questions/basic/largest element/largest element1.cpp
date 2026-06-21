6e234e939ea4fd500ddfead3e0a579d9194bde5b
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-06-21 13:30:20 +0900

    Update largest element1.cpp

--- takeuforward/top array interview questions/basic/largest element/largest element1.cpp
+++ takeuforward/top array interview questions/basic/largest element/largest element1.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int largestElement(vector<int>& nums) {
-        int maximum=0;
+        int maximum=INT_MIN;
         for(int i=0;i<nums.size();i++){
             maximum=max(maximum,nums[i]);
         }
