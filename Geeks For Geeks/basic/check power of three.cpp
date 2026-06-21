3b96e34cb3512643ceb80ac972dd9a10022e2b98
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-06-21 17:02:30 +0900

    Create check power of three.cpp

---
+++ Geeks For Geeks/basic/check power of three.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool isPowerOf3(int n) {
+        return n>0 && 1162261467%n==0;
+    }
+};
+
+//normal
+
+class Solution {
+public:
+    bool isPowerOf3(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
