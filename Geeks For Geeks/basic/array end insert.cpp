ee0336f8d9b02f4b0f26c9cba9775865c4197088
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-07-05 02:46:02 +0900

    Update array end insert.cpp

--- Geeks For Geeks/basic/array end insert.cpp
+++ Geeks For Geeks/basic/array end insert.cpp
@@ -2,5 +2,28 @@ class Solution {
   public:
     void insertAtEnd(vector<int> &arr, int val) {
         arr.push_back(val);
+      // arr.emplace_back(val);
+      //arr.insert(arr.end(), val);
+    }
+};
+
+//other 
+
+class Solution {
+public:
+    void insertAtEnd(vector<int> &arr, int val) {
+        arr.resize(arr.size() + 1); // Expand the size by 1
+        arr.back() = val;           // Assign val to the last element
+    }
+};
+
+//other 2
+
+class Solution {
+public:
+    void insertAtEnd(vector<int> &arr, int val) {
+        int oldSize = arr.size();
+        arr.resize(oldSize + 1);
+        arr[oldSize] = val; // The new element is at the old size index
     }
 };
