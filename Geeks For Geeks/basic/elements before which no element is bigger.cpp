ca03e755708b9f2fa8e85a3d48323ddfab0f604a
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-07-05 17:56:45 +0900

    Update and rename lements before which no element is bigger.cpp to elements before which no element is bigger.cpp

--- Geeks For Geeks/basic/elements before which no element is bigger.cpp
+++ Geeks For Geeks/basic/elements before which no element is bigger.cpp
@@ -1,6 +1,7 @@
 class Solution {
   public:
     int countElements(vector<int>& arr) {
+      if (arr.empty()) return 0;
         int count=1;
         int max=arr[0];
         for(int i=1;i<arr.size();i++){
