2efee57f046af8878e0ee38861b4c44f6bb60c80
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-07-06 03:58:55 +0900

    Update Keep Multiplying Found Values by Two.cpp

--- Leetcode/Daily/Keep Multiplying Found Values by Two.cpp
+++ Leetcode/Daily/Keep Multiplying Found Values by Two.cpp
@@ -1,3 +1,20 @@
+//best
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()){
+            if(nums[i]==original){
+                original*=2;
+                i=0;
+            }
+            else i++;
+        }
+        return original;
+    }
+};
+//my 
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
