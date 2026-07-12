231a5283dbecfed28e6cef3bb5cb99ed3206df05
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2026-07-13 00:31:06 +0900

    Create remove all adjacent duplicates in string.cpp

---
+++ Leetcode/Daily/remove all adjacent duplicates in string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string result="";
+        for(char c:s){
+            if(!result.empty() && result.back()==c){
+                result.pop_back();
+            }else{
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
